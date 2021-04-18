b410173022305914ebbdd3952fade8878f5d38ae
ika25 <lomiika1990@yahoo.com>
2021-04-18 14:28:49 +0100

    Code organization

--- Assets/Scripts/AR Handling/ObjectSpawner.cs
+++ Assets/Scripts/AR Handling/ObjectSpawner.cs
@@ -6,6 +6,7 @@ public class ObjectSpawner : MonoBehaviour
 {
     public GameObject ObjToSpawn;
     private PlacementIndicator PlacementIndicator;
+    bool fingerTouched = false;
     void Start()
     {
         PlacementIndicator = FindObjectOfType<PlacementIndicator>();
@@ -13,7 +14,9 @@ public class ObjectSpawner : MonoBehaviour
 
     void Update()
     {
-        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        fingerTouched = Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began;
+
+        if (fingerTouched)
         {
             GameObject obj = Instantiate(ObjToSpawn, PlacementIndicator.transform.position, PlacementIndicator.transform.rotation);
         }
--- Assets/Scripts/AR Handling/PlacementIndicator.cs
+++ Assets/Scripts/AR Handling/PlacementIndicator.cs
@@ -18,16 +18,21 @@ public class PlacementIndicator : MonoBehaviour
 
     void Update()
     {
-        List<ARRaycastHit> hits = new List<ARRaycastHit>();
-        aRRaycastManager.Raycast(new Vector2(Screen.width/2, Screen.height/2), hits, TrackableType.Planes);
+        PlaceIndicator();
+    }
 
-        if(hits.Count > 0)
+    private void PlaceIndicator()
+    {
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        if (hits.Count > 0)
         {
             this.transform.position = hits[0].pose.position;
             this.transform.rotation = hits[0].pose.rotation;
 
-            if(!placementIndicator.activeInHierarchy)
-            placementIndicator.SetActive(true);
+            if (!placementIndicator.activeInHierarchy)
+                placementIndicator.SetActive(true);
         }
     }
 }
