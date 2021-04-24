4dc729b250991a3b86a3b352563aa94004dbd6dc
ika25 <lomiika1990@yahoo.com>
2021-04-24 08:48:33 +0100

    Adding user names saving functionality
    Main Game is done and fully functional, adding extra feature to try enhance game.

---
+++ Assets/Game/Scripts/Scriptable Objects/UserNames_SoList.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="User names",menuName ="SO list")]
+public class UserNames_SoList : ScriptableObject
+{
+    public List<string> UserNames;
+}
--- Assets/Game/Scripts/UI/UsersControl.cs
+++ Assets/Game/Scripts/UI/UsersControl.cs
@@ -6,13 +6,21 @@ using UnityEngine.UI;
 public class UsersControl : MonoBehaviour
 {
     [SerializeField] GameObject PlayButton;
-    [SerializeField] GameObject UsersDropDown;
+    [SerializeField] Dropdown UsersDropDown;
 
     [SerializeField] Toggle newUserToggle;
     [SerializeField] Toggle existingUserToggle;
 
     [SerializeField] GameObject NewUserInput;
 
+
+    [SerializeField] UserNames_SoList UserNamesList;
+
+    private void Start()
+    {
+        PlayerPrefs.DeleteAll();
+        UserNamesList.UserNames.Clear();
+    }
 
     public void ToggleSet(Toggle toggle)
     {
@@ -21,14 +29,42 @@ public class UsersControl : MonoBehaviour
             if(toggle == newUserToggle)
             {
                 NewUserInput.SetActive(true);
-                UsersDropDown.SetActive(false);
+                UsersDropDown.gameObject.SetActive(false);
             }
             if(toggle == existingUserToggle)
             {
-                UsersDropDown.SetActive(true);
+                LoadUserNames();
+
+                UsersDropDown.gameObject.SetActive(true);
                 PlayButton.SetActive(true);
                 NewUserInput.SetActive(false);
             }
+        }
+    }
+
+
+    private void LoadUserNames()
+    {
+        foreach (var user in UserNamesList.UserNames)
+        {
+            if(!PlayerPrefs.HasKey(user))
+            {
+                PlayerPrefs.SetInt(user, 0);
+            }
+            Dropdown.OptionData newUser = new Dropdown.OptionData(user);
+            if (!UsersDropDown.options.Contains(newUser))
+            {
+                UsersDropDown.options.Add(newUser);
+            }
+        }
+    }
+
+    public void SaveNewUser(InputField newUserNameIF)
+    {
+        if(!PlayerPrefs.HasKey(newUserNameIF.text))
+        {
+            PlayerPrefs.SetInt(newUserNameIF.text, 0);
+            UserNamesList.UserNames.Add(newUserNameIF.text);
         }
     }
 }
