bc5f0c74c7b7ce19407df0b9686fceccd053b77b
ika25 <lomiika1990@yahoo.com>
2021-04-25 19:19:22 +0100

    Final Game

--- Assets/Game/Scripts/AI/GoalScored.cs
+++ Assets/Game/Scripts/AI/GoalScored.cs
@@ -17,8 +17,8 @@ public class GoalScored : MonoBehaviour
     {
         if (other.CompareTag("Ball"))
         {
-            UserNames_SoList.userScores[UsersControl.currentUser] += 10;  //increment score when ball hits the score collider
-            UsersControl.currentScore = UserNames_SoList.userScores[UsersControl.currentUser]; //save this user's score after the increment
+            //UserNames_SoList.userScores[UsersControl.currentUser] += 10;  //increment score when ball hits the score collider
+            //UsersControl.currentScore = UserNames_SoList.userScores[UsersControl.currentUser]; //save this user's score after the increment
             Destroy(other.gameObject, 0.5f);  //destroy ball
             OnGoalScored.Raise(); //fire OnGoalScored event to update the score text
         }
